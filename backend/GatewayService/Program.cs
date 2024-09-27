154f24c7ce3d25c10e41f7fc6cc0bccfa5d94411
Mehran <mehransurface@outlook.com>
2024-09-28 00:02:15 +0330

    hotfix: add CORS policy to GatewayService to allow requests from frontend

--- backend/GatewayService/Program.cs
+++ backend/GatewayService/Program.cs
@@ -3,14 +3,28 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("GatewayCorsPolicy", builder =>
+    {
+        builder.WithOrigins(["http://localhost:3000"]) // Specify the allowed origin(s)
+             .AllowAnyMethod()
+             .AllowAnyHeader()
+             .AllowCredentials(); // Adjust the policy according to your needs
+    });
+});
 // Add Ocelot Configuration
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
 
 // Register Ocelot services
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
+app.UseCors("GatewayCorsPolicy");
 // Configure Ocelot middleware
 app.UseOcelot().Wait();
 
